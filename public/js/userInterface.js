a0b577763fa86da2cca38f3feb4fc232d140cbab
adoshulgin <shulgin.ado@gmail.com>
2014-05-16 21:30:52 +0600

    основное приложение

--- public/js/userInterface.js
+++ public/js/userInterface.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
     $('#bigButton').click(function () {
         $("#bigButton").css("display", "none");
+        $("#step1").css("display", "block");
+    });
+
+    $('#button').click(function () {
+        $("#button").css("display", "none");
+        $("#step1").css("display", "none");
+        $("#step2").css("display", "block");
     });
 
 });
